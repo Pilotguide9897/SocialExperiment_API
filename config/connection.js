ae1610194a1bc41d324c13bf50b3ae9381f11a96
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-05-21 11:54:18 -0300

    server running and connecting to mongo

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
 // Wrap Mongoose around local connection to MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/socialNetworkDB", () => {
-    console.log("Connected to Mongo")
-},
-e => console.error(e)
-);
+mongoose
+  .connect("mongodb://127.0.0.1:27017/socialNetworkDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to Mongo"))
+  .catch((e) => console.error(e));
 
 // Export connection
 module.exports = mongoose.connection;
