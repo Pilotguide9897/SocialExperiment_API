01e17ccc411dc3c7133870f78d24690064443357
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-05-23 20:05:57 -0300

    reactions renderring correctly now. The problem was with an incorrect import path

--- models/thought.js
+++ models/thought.js
@@ -1,38 +1,43 @@
-const mongoose = require('mongoose');
-const reactionSchema = require('../models/reactions');
+const mongoose = require("mongoose");
+const reactionSchema = require("./reactions");
 
-const thoughtsSchema = new mongoose.Schema({
+const thoughtsSchema = new mongoose.Schema(
+  {
     thoughtText: {
-        type: String,
-        required: true,
-        minLength: 1,
-        maxLength: 280
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
     },
     createdAt: {
-        type: Date,
-        default: Date.now,
-        get: function (value) {
-            const month = value.getMonth() + 1;
-            const day = value.getDate();
-            const year = value.getFullYear();
-            return `${month}/${day}/${year}`;
-        }
+      type: Date,
+      default: Date.now,
+      get: function (value) {
+        const month = value.getMonth() + 1;
+        const day = value.getDate();
+        const year = value.getFullYear();
+        const hours = value.getHours();
+        const minutes = ("0" + value.getMinutes()).slice(-2); 
+        return `${month}/${day}/${year} at ${hours}:${minutes}`;
+      },
     },
     username: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    reactions: [reactionSchema]
-}, {
+    reactions: [reactionSchema],
+  },
+  {
     toJSON: { virtuals: true, getters: true },
     toObject: { virtuals: true, getters: true },
-    id: false
+    id: false,
+  }
+);
+
+thoughtsSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
 });
 
-thoughtsSchema.virtual('reactionCount').get(function () {
-    return this.reactions.length;
-});
-
-const Thoughts = mongoose.model('Thoughts', thoughtsSchema);
+const Thoughts = mongoose.model("Thoughts", thoughtsSchema);
 
 module.exports = Thoughts;
